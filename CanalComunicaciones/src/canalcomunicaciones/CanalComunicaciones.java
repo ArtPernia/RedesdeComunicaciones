c81228864f21f52c6f86106c312e77c04d7cd074
Khalifa96 <37151079+Khalifa96@users.noreply.github.com>
2018-10-31 09:38:34 +0100

    canal

--- CanalComunicaciones/src/canalcomunicaciones/CanalComunicaciones.java
+++ CanalComunicaciones/src/canalcomunicaciones/CanalComunicaciones.java
@@ -10,35 +10,17 @@ public static Double PROB_ERR = 0.5;
 public static int TAM_BYTE = 8;
 
     public static void main(String[] args) throws IOException{
-        int[] b = new int[TAM_BYTE];
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] charArray = br.readLine().toCharArray();
-        
-        for(int i=0; i<charArray.length; i++){
-            b[i] = (int)(charArray[i]);
+        InputStream in = System.in;
+        while(true){
+            int carac = in.read();
+            System.out.println("I:" + (char)carac+":" + carac + ":" + Integer.toBinaryString(carac));
+            Double errorByte = Math.random();
+            if(errorByte<PROB_ERR){
+            Random rnd = new Random();
+              int index = rnd.nextInt(TAM_BYTE)-1;
+                carac = carac^(1<<index);
+            }
+            System.out.println("O:" + (char)carac+":" + carac + ":" + Integer.toBinaryString(carac));
         }
-        for(int i : b){
-            if(i==49)
-                i=1;
-            else if(i==48)
-                i=0;
-        }
-        Double errorByte = Math.random();
-        Random rnd = new Random();
-        int index = rnd.nextInt(TAM_BYTE);
-        if(errorByte<PROB_ERR){
-            if(b[index]==49)
-                b[index]=48;
-            else if(b[index]==48)
-                b[index]=49;
-        }
-        
-        for(int i=0; i<b.length; i++){
-            charArray[i] = (char)(b[i]);
-        }
-        
-        System.out.println(new String(charArray));
-        
     }
 }
