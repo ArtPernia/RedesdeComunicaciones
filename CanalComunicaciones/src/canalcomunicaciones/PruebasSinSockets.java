1212dfcb552a8bd1f7896a656c54863a4a66d91f
miguelvitores <34660797+miguelvitores@users.noreply.github.com>
2018-11-01 02:21:12 +0100

    desplazamiento
                _  _
               (.)(.)
              /  ()  \
            _ \ '--' / _
           { '-`""""`-' }
            `"/      \"`
              \      /
             _/  /\  \_
            {   /  \   }
             `"`    `"`

--- CanalComunicaciones/src/canalcomunicaciones/PruebasSinSockets.java
+++ CanalComunicaciones/src/canalcomunicaciones/PruebasSinSockets.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package canalcomunicaciones;
 
 import java.io.*;
@@ -11,31 +6,58 @@ import java.util.Random;
 
 public class PruebasSinSockets {
     
-    public static Double PROB_ERR_BIT = 0.5;
-    public static Double PROB_ERR_OFFSET = 0.25;
+    public static double PROB_ERR_BIT = 0.05;
+    public static double PROB_ERR_DESP = 0.75;
     public static int TAM_BYTE = 8;
     public static void main(String[] args) throws IOException, Exception{
         InputStream in = System.in;
         int carac[] = new int[2];
+        Random rnd = new Random();
+        boolean salir = false;
         // & sale de la funcion
-        while((carac[0] = in.read()) != '-' && (carac[1] = in.read()) != '1'){
+        while( !salir ){
+            
+            System.out.println("----------------------------------------------");
+            for(int i=0; i<carac.length; i++){
+                carac[i] = in.read();
+                if(carac[i] == 38 )
+                    salir = true;
+            }
+            
             for(int c : carac)
-                System.out.println("I:" + (char)c+":" + c + ":" + Integer.toBinaryString(c));
-            Double errorBit = Math.random();
-            for(int c : carac){
-                if(errorBit<PROB_ERR_BIT){
-                    Random rnd = new Random();
-                    int index = rnd.nextInt(TAM_BYTE)-1;
-                    c = c^(1<<index);
+                System.out.println("I:" + (char)c + ":" + c + ":" + Integer.toBinaryString(c));
+            for(int i=0; i<carac.length; i++){
+                if(Math.random() < PROB_ERR_BIT){
+                    int index = rnd.nextInt(TAM_BYTE);
+                    System.out.println("Error en el bit " + (index+1) + " del caracter " + (char)carac[i]);
+                    carac[i] = carac[i] ^ (1 << index);
                 }
             }
-            //Concatenar dos strings con tobynary y hacer valueof del resultado, aplicar el desplazamiento
-            if(Math.random()<PROB_ERR_OFFSET){
-                //int totalCarac = 
+            
+            double despError = PROB_ERR_DESP;
+            int despCont = 0;
+            while(Math.random() < (despError *= PROB_ERR_DESP))
+                despCont++;
+            if(despCont > 0){
+                int concCarac = carac[0] | (carac[1] << TAM_BYTE );
+                System.out.println("Concatenados antes de error: "+Integer.toBinaryString(concCarac));
+                boolean desplazaDerecha = rnd.nextBoolean();
+                if(desplazaDerecha){
+                    concCarac = concCarac >>> despCont;
+                    System.out.println("Desplazamiento sin signo derecha " + despCont + " bits");
+                }
+                else{
+                    concCarac = concCarac << despCont;
+                    System.out.println("Desplazamiento izquierda " + despCont + " bits");
+                }
+                System.out.println("Concatenados después de error: "+Integer.toBinaryString(concCarac));
+                carac[1] = concCarac >>> TAM_BYTE;
+                carac[0] = concCarac ^ (carac[1] << TAM_BYTE);
             }
-           for(int c : carac)
-                System.out.println("O:" + (char)c+":" + c + ":" + Integer.toBinaryString(c));
-            //outSalida.writeObject(carac);
+            
+            for(int c : carac)
+                System.out.println("O:" + (char)c + ":" + c + ":" + Integer.toBinaryString(c));
+            
         }
     }
 }
