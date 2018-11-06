838373cc77b032fa70b2d8f958408433ce1fc2a7
jose doyague <josedoy97@gmail.com>
2018-11-06 13:00:50 +0100

    Update hamming.java

--- CanalComunicaciones/hamming.java
+++ CanalComunicaciones/hamming.java
@@ -1,26 +1,15 @@
 package canalcomunicaciones;
 
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
+import java.math.*;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.*;
-
-<<<<<<< HEAD
-class DanHamming {
-=======
-class Hamming {
->>>>>>> parent of 9c5c3d8... a tomar viento
-
-    public static void main(String arg[]) throws IOException {
+ class DanHamming {
+     public static void main(String arg[]) throws IOException {
         /**/
         try {
             InputStream is = System.in;
             OutputStream os = System.out;
-<<<<<<< HEAD
             int caracter_leido;
             int caracter_previo = 0;
           
@@ -36,95 +25,22 @@ class Hamming {
                     procesar_doscaracteres(caracter_previo, caracter_leido);
                     if(vacio(caracteres_enviados)){
                         
-                    }
-                    
+                    }                    
                     flag = 0;
                 }else{
                     flag++;
-                }
-               
+                }      
                 caracter_previo = caracter_leido; 
            
             }
-            
-            
-            
-//            }
-=======
-            byte[] buffer = new byte[64];
-            int nb;
-            char[] bufferChar = new char[2];
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            br.read(bufferChar, 0, 1);
-                    
-            while (is.available() > 0) {
-                nb = is.read(buffer);
-                if (nb > 0) {
-                    //1er Arg el buffer, el 2 desde donde empiezo a leer, 3 cuantos bytes leo
-                    baos.write(buffer, 0, nb);
-                }
-            }
->>>>>>> parent of 9c5c3d8... a tomar viento
+
         } catch (Exception ex) {
             System.out.println("Error: " + ex.toString());
         }
-
-<<<<<<< HEAD
+     
         
-        
-        
-=======
->>>>>>> parent of 9c5c3d8... a tomar viento
-        /*Lectura y correccion*/
-        System.out.println("Enter the Received codeword");
-        int r[] = new int[12];
-        for (int i = 0; i < 12; i++) {
-           // r[i] = sc.nextInt();
-        }
-
-        int pr[] = new int[4];
-        int rd[] = new int[8];
-
-        pr[0] = r[0];
-        pr[1] = r[1];
-        rd[0] = r[2];
-        pr[2] = r[3];
-        rd[1] = r[4];
-        rd[2] = r[5];
-        rd[3] = r[6];
-        pr[3] = r[7];
-        rd[4] = r[8];
-        rd[5] = r[9];
-        rd[6] = r[10];
-        rd[7] = r[11];
-        int s[] = new int[4];
-        s[0] = pr[0] ^ rd[0] ^ rd[1] ^ rd[3] ^ rd[4] ^ rd[6];
-        s[1] = pr[1] ^ rd[0] ^ rd[2] ^ rd[3] ^ rd[5] ^ rd[6];
-        s[2] = pr[2] ^ rd[1] ^ rd[2] ^ rd[3];
-        s[3] = pr[3] ^ rd[4] ^ rd[5] ^ rd[6] ^ rd[7];
-
-        int dec = (s[0] * 1) + (s[1] * 2) + (s[2] * 4) + (s[3] * 8);
-        if (dec == 0) {
-            System.out.println("Sin error");
-        } else {
-            System.out.println("El error esta en: " + dec);
-            if (r[dec - 1] == 0) {
-                r[dec - 1] = 1;
-            } else {
-                r[dec - 1] = 0;
-
-            }
-            System.out.println(
-                    "Corrected code word is : ");
-            for (int i = 0; i < 12; i++) {
-                System.out.print(r[i] + " ");
-            }
-            System.out.println();
-        }
     }
-
-    private static int[] codigoHamming(int a) {
+     private static int[] codigoHamming(int a) {
         int d[] = new int[8];
         int p[] = new int[4];             
         for (int i=0;i<7; i++){
@@ -132,11 +48,11 @@ class Hamming {
             a = a >> 1;
             i--;      
         }
-        p[0] = d[0] ^ d[1] ^ d[3] ^ d[4] ^ d[6];
-        p[1] = d[0] ^ d[2] ^ d[3] ^ d[5] ^ d[6];
-        p[2] = d[1] ^ d[2] ^ d[3];
-        p[3] = d[4] ^ d[5] ^ d[6] ^ d[7];
-        int c[] = new int[12];
+        p[0] = d[0] ^ d[1] ^ d[2] ^ d[3] ^ d[4] ^ d[5] ^ d[6] ^ d[7];
+        p[1] = d[0] ^ d[1] ^ d[2] ^ d[3];
+        p[2] = d[0] ^ d[1] ^ d[4] ^ d[5];
+        p[3] = d[0] ^ d[2] ^ d[4] ^ d[6];
+        int c[] = new int[13];
         System.out.println("Complet code word: ");
         c[0] = p[0];
         c[1] = p[1];
@@ -150,17 +66,16 @@ class Hamming {
         c[9] = d[5];
         c[10] = d[6];
         c[11] = d[7];
-        /*Salida de datos*/
-
-        for (int i = 0; i < 12; i++) {
+        
+        
+        /*Salida de datos por pantalla*/
+         for (int i = 0; i < 12; i++) {
             System.out.print(c[i] + "");
         }
         System.out.println();
-
-        return c;
+         return c;
     }
     
-<<<<<<< HEAD
     public static  boolean vacio (int a[] ){// devuelve true si array vacio
         for  (int i = 0; i <=  a.length; i++){
             if(a[i] == -1 ){
@@ -177,8 +92,7 @@ class Hamming {
     }
     
     private static int [] procesar_doscaracteres(int leido,int previo){
-
-        return concatenateCode(codigoHamming(previo),codigoHamming(leido)) ;
+         return concatenateCode(codigoHamming(previo),codigoHamming(leido)) ;
     }
     
     private static int[] concatenateCode (int a[], int b[]){  
@@ -192,13 +106,36 @@ class Hamming {
     }
     
     private static int[] binaryToDecimal (int a[]){
-      /*convertir un array de 24 bits a 3 numeros de tipo int*/
-        return 
+     
+        int [] arrayDec = new int [3];
+        int[] letraA = new int[8];
+        int[] letraB = new int[8];
+        int[] letraC = new int[8];
+        int sumA, sumB, sumC;
+        //              (src   , src-offset  , dest , offset, count)
+        System.arraycopy(a, 0           , letraA, 0     , letraA.length);
+        System.arraycopy(a, letraA.length, letraB, 0     , letraB.length);
+        System.arraycopy(a, letraB.length, letraC, 0, letraC.length);
+        
+        sumA = decimal(letraA);
+        sumB = decimal(letraB);
+        sumC = decimal(letraC);
+        
+        arrayDec[0] = sumA;
+        arrayDec[1] = sumB;
+        arrayDec[2] = sumC;
+        
+       
+        return arrayDec;
+        
     }
-    
+private static int decimal (int [] a){
+    int c=0;
+    for (int i = 0; i<8 ; i++){
+        c = c + (int) (a[i]* Math.pow(10, i));   
+    }
+         
+    return c;
+}
     
 }/*END CLASS*/
-
-=======
-}/*END CLASS*/
->>>>>>> parent of 9c5c3d8... a tomar viento
