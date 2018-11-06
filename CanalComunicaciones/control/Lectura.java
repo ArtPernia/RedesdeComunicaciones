38b5b86453739c05c233ce45dd618cf59a9ae5df
juancarrascogz <44373169+juancarrascogz@users.noreply.github.com>
2018-11-06 11:19:23 +0100

    Add files via upload

---
+++ CanalComunicaciones/control/Lectura.java
@@ -0,0 +1,43 @@
+
+package control;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Lectura extends Thread {
+    private String ruta="textoPrueba.txt";
+    
+    public void run(){
+    if(ruta==null){
+        System.out.println("No se ha introducido la ruta");
+    }else{
+    int carac;    
+    FileReader f1;
+        try {
+            f1 = new FileReader(ruta);
+            carac = f1.read();
+            while(carac != -1){
+                HiloSocket h1 = new HiloSocket(carac);
+                h1.start();
+                
+                carac = f1.read();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Lectura.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lectura.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lectura.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        }
+    }
+}
