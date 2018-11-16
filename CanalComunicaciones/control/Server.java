6678adfea246bb9fd3b393d09bbdda660c146d52
juancarrascogz <44373169+juancarrascogz@users.noreply.github.com>
2018-11-16 12:48:20 +0100

    Add files via upload

--- CanalComunicaciones/control/Server.java
+++ CanalComunicaciones/control/Server.java
@@ -8,6 +8,11 @@ public class Server {
     public static void main(String[] args) throws IOException {
         
         ServidorSocket s1 = new ServidorSocket(6969);
+        
+        while(true){
+            System.out.println(s1.leer());
+        }
+        
     }
     
 }
--- CanalComunicaciones/control/ServidorSocket.java
+++ CanalComunicaciones/control/ServidorSocket.java
@@ -1,13 +1,11 @@
 
 package control;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.Charset;
 
 public class ServidorSocket { 
            
@@ -17,43 +15,26 @@ public class ServidorSocket {
     
     public ServidorSocket(int port){
         this.port=port;
-        this.init();
-    }
     
-    public void init(){
         ServerSocket server = null;
         
         try{            
         server = new ServerSocket(port);
-        while(true){
-//        System.out.println("Escuchando... en el puerto: "+port);                             
+        
         Socket cliente = server.accept(); 
         
-            is = cliente.getInputStream(); 
-            os = cliente.getOutputStream();
+            this.is = cliente.getInputStream(); 
+            this.os = cliente.getOutputStream();
             
-        byte [] buffer = new byte[1024];
-            int nb = -1;
-            
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        }                                
         }catch(Exception ex){
             System.out.println("Error: " + ex.toString());
         }
     }
     
     public int leer() throws IOException{
-        
-        while(is.available()>0){
-                return is.read();
-        }
-        return -1;
+        return is.read();
     }
-            
-//            String mensaje = new String (baos.toByteArray(), Charset.forName("UTF-8"));
-//            System.out.print(mensaje);
-            
-     
+      
     public void desconectar() throws IOException{
         os.close();
     }
