24b73aaa458d5c32f7520c0d9d1ca670cfc02f8b
alexmaestroUEMC <amaestro8013@alumnos.uemc.es>
2018-12-04 13:59:59 +0100

    primer paso

--- CanalComunicaciones/src/Practica2/Practica2.java
+++ CanalComunicaciones/src/Practica2/Practica2.java
@@ -1,8 +1,25 @@
 package Practica2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practica2 {
  
     public static void main (String args){
         
+        Map<Integer,String> mensaje = new HashMap<>();
+        //Map<Integer,String> mensaje = new HashMap<Integer, String>();
+        
+        mensaje.put(8,"Fix 4.1");
+        String cadena;
+        while (){
+            int caracter = (char) Socket.leer();
+            if caracter == DELIM){
+                procesar(cadena);
+            }
+            else {
+                cadena.append(caracter);
+            }
+        }
     }
 }
